2621c6e446017c4766a069480dbd5b53976496e8
Ahmed-Abulmagd <60317344+Ahmed-Abulmagd@users.noreply.github.com>
2022-08-20 08:26:47 +0200

    add code

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ahmed Abuelmagd on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
